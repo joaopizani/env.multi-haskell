13994db127ae1232eb4b6f46ae460f4159da161e
João Paulo Pizani Flor <joaopizani@gmail.com>
2015-08-03 10:15:05 +0200

    Parameterize file with PATH definitions

--- install-basic.sh
+++ install-basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# multi-haskell install script: adds content to the user's ~/.profile in order
+# multi-haskell install script: adds content to the user's path in order
 # to set an environment variable (MULTIHASKELL_ENVS) identifying the root
 # directory where all Haskell installations live. Also, add the proper bin
 # directories to the user's PATH, so that he/she has a properly functioning
@@ -13,12 +13,16 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 # Where all haskell environments (GHC + Cabal + (optional) Platform) live
 MULTIHASKELL_ENVS_DEFAULT_="${DIR}/envs"  # By default a subdir of the repo itself
 MULTIHASKELL_ENVS__=${1:-"$MULTIHASKELL_ENVS_DEFAULT_"}  # But can be given as argument
-MULTIHASKELL_ENVS_="$(cd -P "$(readlink -f "${MULTIHASKELL_ENVS__}")" && pwd)"  # absolute
+MULTIHASKELL_ENVS_="$(readlink -m "${MULTIHASKELL_ENVS__}")"  # absolute
+
+PATHVARS_FILE_DEFAULT_="${HOME}/.bashrc"
+PATHVARS_FILE_="${2:-"${PATHVARS_FILE_DEFAULT_}"}"
+PATHVARS_FILE="$(readlink -m "${PATHVARS_FILE_}")"
+
 
 mkdir -p "${MULTIHASKELL_ENVS_}"  # makes sure that the envs tree exists
 
-# Extending PATH to enable the Haskell toolchain as well as the multi-haskell scripts themselves
-cat <<EOF >> "${HOME}/.profile"
+cat <<EOF >> "${PATHVARS_FILE}"
 
 ### MULTI-HASKELL section (http://github.com/joaopizani/multi-haskell) ###
 
