f43b1eb429ff4123e09c7769a6f56360b4798c64
João Paulo Pizani Flor <joaopizani@gmail.com>
2015-08-09 18:05:21 +0200

    Have a self-contained file with PATH assignments and source it from bashrc

--- install-basic.sh
+++ install-basic.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # multi-haskell install script: adds content to the user's path in order
 # to set an environment variable (MULTIHASKELL_ENVS) identifying the root
@@ -15,17 +14,15 @@ MULTIHASKELL_ENVS_DEFAULT_="${DIR}/envs"  # By default a subdir of the repo itse
 MULTIHASKELL_ENVS__=${1:-"$MULTIHASKELL_ENVS_DEFAULT_"}  # But can be given as argument
 MULTIHASKELL_ENVS_="$(readlink -m "${MULTIHASKELL_ENVS__}")"  # absolute
 
-PATHVARS_FILE_DEFAULT_="${HOME}/.bashrc"
-PATHVARS_FILE_="${2:-"${PATHVARS_FILE_DEFAULT_}"}"
-PATHVARS_FILE="$(readlink -m "${PATHVARS_FILE_}")"
+PATHVARS_FILE="$(readlink -m "${DIR}/.multi-haskell-path-vars")"
+BASHRC_FILE_DEFAULT_="${HOME}/.bashrc"
+BASHRC_FILE_="${2:-"${BASHRC_FILE_DEFAULT_}"}"
+BASHRC_FILE="$(readlink -m ${BASHRC_FILE_})"
 
 
 mkdir -p "${MULTIHASKELL_ENVS_}"  # makes sure that the envs tree exists
 
 cat <<EOF >> "${PATHVARS_FILE}"
-
-### MULTI-HASKELL section (http://github.com/joaopizani/multi-haskell) ###
-
 export MULTIHASKELL_ENVS="${MULTIHASKELL_ENVS_}"
 
 PATH="${DIR}/bin:\${PATH}"
@@ -35,4 +32,6 @@ PATH="\${MULTIHASKELL_ENVS}/current/cabal/bin:\${PATH}"
 PATH=".cabal-sandbox/bin:\${PATH}"
 export PATH
 EOF
+
+echo -e "\n\n. ${PATHVARS_FILE}" >> "${BASHRC_FILE}"
 
